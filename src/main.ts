d150d4d40f7728fda3e5688341d23badf03300a3
kifuan <kifuan@foxmail.com>
2023-04-04 12:31:50 +0800

    feat: icon `powerCon[1|5]`

--- src/main.ts
+++ src/main.ts
@@ -11,39 +11,20 @@ const AMBIENT_COLOR = 0x222222
 const options = {
   lightSpeed: 0.1,
   rotateDirection: -1,
+  showIcon: true,
   github: () => {
     window.open('https://github.com/kifuan/cyber-sundial')
   },
 }
 
-function createObjects(scene: THREE.Scene) {
-  const material = new THREE.MeshStandardMaterial({ color: WHITE_COLOR })
+const gui = new GUI({
+  title: 'Options',
+})
 
-  const panelGeo = new THREE.CylinderGeometry(PANEL_RADIUS, PANEL_RADIUS, 1, 150)
-  const panelMesh = new THREE.Mesh(panelGeo, material)
-  panelMesh.position.set(0, 0, 0)
-  panelMesh.receiveShadow = true
-  scene.add(panelMesh)
-
-  const poleGeo = new THREE.CylinderGeometry(0.5, 0.5, 16)
-  const poleMesh = new THREE.Mesh(poleGeo, material)
-  poleMesh.position.set(0, 8, 0)
-  poleMesh.castShadow = true
-  scene.add(poleMesh)
-
-  // Scales
-  for (let i = 0; i < 12; i++) {
-    const scaleGeo = new THREE.BoxGeometry(0.5, 0.5, PANEL_RADIUS / 4)
-    const scaleMat = new THREE.MeshStandardMaterial({ color: SCALE_COLOR })
-    const scaleMesh = new THREE.Mesh(scaleGeo, scaleMat)
-
-    const angle = i * Math.PI / 6 // Divide circle into 12 parts
-    scaleMesh.position.set(Math.cos(angle) * PANEL_RADIUS * 0.85, 0.5, Math.sin(angle) * PANEL_RADIUS * 0.85)
-    scaleMesh.lookAt(0, 0, 0)
-
-    scene.add(scaleMesh) // Add the scale to the panel mesh
-  }
-}
+gui.add(options, 'lightSpeed', 0.1, 10).name('Speed').listen()
+gui.add(options, 'rotateDirection', { Clockwise: -1, AntiClockwise: 1 }).name('Direction').listen()
+gui.add(options, 'showIcon').name('Show Icon').listen()
+gui.add(options, 'github').name('GitHub')
 
 function createNormalLights(scene: THREE.Scene) {
   scene.add(new THREE.AmbientLight(AMBIENT_COLOR))
@@ -70,16 +51,6 @@ function createSpotLight(scene: THREE.Scene): THREE.Object3D {
   return container
 }
 
-function createGUI() {
-  const gui = new GUI({
-    title: 'Options',
-  })
-
-  gui.add(options, 'lightSpeed', 0.1, 10).name('Speed').listen()
-  gui.add(options, 'rotateDirection', { Clockwise: -1, AntiClockwise: 1 }).name('Direction').listen()
-  gui.add(options, 'github').name('GitHub')
-}
-
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
 if (window.innerWidth <= window.innerHeight)
   camera.position.set(20, 70, 70)
@@ -101,10 +72,51 @@ const scene = new THREE.Scene()
 scene.background = new THREE.Color(BG_COLOR)
 scene.fog = new THREE.FogExp2(BG_COLOR, 0.002)
 
-// Create objects.
-createObjects(scene)
+// Objects.
+const material = new THREE.MeshStandardMaterial({ color: WHITE_COLOR })
+
+const panelGeo = new THREE.CylinderGeometry(PANEL_RADIUS, PANEL_RADIUS, 1, 150)
+const panelMesh = new THREE.Mesh(panelGeo, material)
+panelMesh.position.set(0, 0, 0)
+panelMesh.receiveShadow = true
+scene.add(panelMesh)
+
+const poleGeo = new THREE.CylinderGeometry(0.5, 0.5, 16)
+const poleMesh = new THREE.Mesh(poleGeo, material)
+poleMesh.position.set(0, 8, 0)
+poleMesh.castShadow = true
+scene.add(poleMesh)
+
+// Icon
+const iconMat = new THREE.MeshStandardMaterial({
+  map: new THREE.TextureLoader().load('icon.svg'),
+  transparent: true,
+  color: WHITE_COLOR,
+  side: THREE.FrontSide,
+})
+const iconGeo = new THREE.BoxGeometry(PANEL_RADIUS / 2, 0.1, PANEL_RADIUS / 6)
+const iconMesh = new THREE.Mesh(iconGeo, iconMat)
+iconMesh.receiveShadow = true
+iconMesh.position.set(0, 0.5, PANEL_RADIUS / 4)
+iconMesh.scale.set(2, 0.1, 2)
+scene.add(iconMesh)
+
+// Scales
+for (let i = 0; i < 12; i++) {
+  const scaleGeo = new THREE.BoxGeometry(0.5, 0.5, PANEL_RADIUS / 4)
+  const scaleMat = new THREE.MeshStandardMaterial({
+    color: SCALE_COLOR,
+  })
+  const scaleMesh = new THREE.Mesh(scaleGeo, scaleMat)
+
+  const angle = i * Math.PI / 6 // Divide circle into 12 parts
+  scaleMesh.position.set(Math.cos(angle) * PANEL_RADIUS * 0.85, 0.5, Math.sin(angle) * PANEL_RADIUS * 0.85)
+  scaleMesh.lookAt(0, 0, 0)
+
+  scene.add(scaleMesh) // Add the scale to the panel mesh
+}
+
 createNormalLights(scene)
-createGUI()
 const spotLight = createSpotLight(scene)
 
 document.addEventListener('resize', () => {
@@ -117,6 +129,7 @@ const clock = new THREE.Clock()
 
 function animate() {
   requestAnimationFrame(animate)
+  iconMesh.visible = options.showIcon
   spotLight.rotateY(clock.getDelta() * options.lightSpeed * options.rotateDirection)
   renderer.render(scene, camera)
 }
